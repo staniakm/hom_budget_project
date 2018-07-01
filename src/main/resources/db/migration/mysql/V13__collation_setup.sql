451e5e9b3c26c7deff4f2cde308aa313fab3379a
Mariusz <!Shadow83>
2018-07-01 16:24:14 +0200

    fix tables collation to polish

---
+++ src/main/resources/db/migration/mysql/V13__collation_setup.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `verification_tokens` DEFAULT CHARSET=utf8 COLLATE utf8_polish_ci;
+ALTER TABLE `income` DEFAULT CHARSET=utf8 COLLATE utf8_polish_ci;
+ALTER TABLE `expense` DEFAULT CHARSET=utf8 COLLATE utf8_polish_ci;
