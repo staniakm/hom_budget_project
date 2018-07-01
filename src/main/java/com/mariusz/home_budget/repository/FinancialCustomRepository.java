4e871d8c6ce9080402566e6e547669772f5a9129
Mariusz Staniak <mariusz.staniak@gmail.com>
2018-07-01 16:30:21 +0200

    suppress warnings for sql query

--- src/main/java/com/mariusz/home_budget/repository/FinancialCustomRepository.java
+++ src/main/java/com/mariusz/home_budget/repository/FinancialCustomRepository.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@SuppressWarnings("ALL")
 @Repository
 public class FinancialCustomRepository {
     private final JdbcTemplate jdbcTemplate;
